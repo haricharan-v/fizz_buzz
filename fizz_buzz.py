6b2bebe25fb062d13563519a53013f545b58cbf7
haricharan-v <66981624+haricharan-v@users.noreply.github.com>
2023-11-23 21:03:46 -0500

    Add files via upload

---
+++ fizz_buzz.py
@@ -0,0 +1,17 @@
+def fizzbuzz(n: int):
+    result = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(str(i))
+    return result
+
+# Taking user input for n
+n = int(input("Enter a number: "))
+output = fizzbuzz(n)
+print(output)
